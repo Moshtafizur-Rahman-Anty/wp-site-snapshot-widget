679a85404bc9a2889b811920ef1121088fada679
Moshtafizur-Rahman-Anty <alphaaa.90@gmail.com>
2026-05-26 23:57:31 +0600

    class-wp-site-snapshot.php file added

--- includes/class-wp-site-snapshot.php
+++ includes/class-wp-site-snapshot.php
@@ -1,5 +1,27 @@
 <?php 
 
+if(!defined('ABSPATH')) {
+    EXIT;
+}
 
 
-?>
+if(!class_exist('WP_Site_Snapshot')) {
+
+    class WP_Site_Snapshot {
+
+        public function __construct() {
+            add_action("wp-dashboard_setup", [$this, "register_dashboard"]);
+        }
+
+        public function register_dashboard() {
+            wp_add_dashboard_widget("wp_site_snapshot_widget", esc_html__( "WP Site Snapshot", "wp-site-snapshot", [$this, "render_widget_html"] ));
+        }
+
+        public function render_widget_html() {
+                
+        }
+
+    }
+
+
+}
