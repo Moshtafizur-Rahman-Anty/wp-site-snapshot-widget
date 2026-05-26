b576a6d87feed215d17bfe085cd3a6971e5736d6
Moshtafizur-Rahman-Anty <alphaaa.90@gmail.com>
2026-05-27 00:16:30 +0600

    fixed wp_add_dashboard_widget() function parameters

--- includes/class-wp-site-snapshot.php
+++ includes/class-wp-site-snapshot.php
@@ -15,7 +15,7 @@ if(!class_exists('WP_Site_Snapshot')) {
         }
 
         public function register_dashboard() {
-            wp_add_dashboard_widget("wp_site_snapshot_widget", esc_html__( "WP Site Snapshot", "wp-site-snapshot", [$this, "render_widget_html"] ));
+            wp_add_dashboard_widget("wp_site_snapshot_widget", esc_html__( "WP Site Snapshot", "wp-site-snapshot"), [$this, "render_widget_html"]);
         }
 
         public function render_widget_html() {
