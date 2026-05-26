4c5f21ca155cb0aed65a8c01e5ff46abf0819006
Moshtafizur-Rahman-Anty <alphaaa.90@gmail.com>
2026-05-26 23:44:57 +0600

    added the wp-ste-snapshot.php oop

--- wp-site-snapshot.php
+++ wp-site-snapshot.php
@@ -9,3 +9,20 @@
  * Text Domain: wp-site-snapshot
  */
 
+if(!defined('ABSPATH')) {
+    exit; // exit of accessed directly
+}
+
+///define the constant
+define("WP_SITE_SNAPSHOT_PATH", plugin_dir_path( __FILE__ ));
+
+
+//include the main class
+require_once WP_SITE_SNAPSHOT_PATH . 'includes/class-wp-site-snapshot.php';
+
+function wp_site_snapshot_plugin(){
+    new WP_Site_Snapshot();
+}
+
+
+add_action('plugins_loaded', 'wp_site_snapshot_plugin'); 
