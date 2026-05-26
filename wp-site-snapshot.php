6b9b79968a69ec40301f2c8f32d9d9901ebc8b72
Moshtafizur-Rahman-Anty <alphaaa.90@gmail.com>
2026-05-26 23:35:09 +0600

    added header to the main php file

---
+++ wp-site-snapshot.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: WP Site Snapshot
+ * Description: See your data instantly. Adds a dashboard widget with post counts, comment totals, and your latest 5 article titles
+ * Version: 1.0.0
+ * Author: Moshtafizur
+ * License: GPL-2.0-or-later
+ * Text Domain: wp-site-snapshot
+ */
+
